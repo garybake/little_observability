77eb30ac4b29a3f6c79252f7ad53a94cb768a1e3
Gary Bake <garybake@gmail.com>
2022-12-22 08:49:50 +0000

    Pull consumption from db

--- hub/api/energy.py
+++ hub/api/energy.py
@@ -1,18 +1,17 @@
 import os
-from typing import Any, List
+from typing import Any, List, Union
 
 from fastapi import APIRouter
 
-# from app import schemas
-# from app.models import LocationKey
-
 from schemas.energy import ConsumptionResponse
-from models.energy import  EnergyConsumption
+from models.energy import EnergyConsumption
+
 
 router = APIRouter()
 
 @router.get("/electricity_consumption", response_model=List[ConsumptionResponse])
-async def read_energy_consumption():
-    resp = EnergyConsumption.get_electricity_consumption()
+async def read_energy_consumption(count: Union[int, None] = None) -> ConsumptionResponse:
+    resp = EnergyConsumption.get_electricity_consumption(count=count)
+    print(f"count: {count}")
 
     return resp
--- hub/models/energy.py
+++ hub/models/energy.py
@@ -1,16 +1,19 @@
 from typing import List
 
+from core.db import DB
 from schemas.energy import ConsumptionResponse
 
 
 class EnergyConsumption:
 
   @staticmethod
-  def get_electricity_consumption() -> List[ConsumptionResponse]:
-    consumption = [{
-        'product': 'Electricity',
-        'interval_start': 1671132600.0,
-        'interval_end': 1671134400.0,
-        'consumption': 0.1253,
-    }]
+  def get_electricity_consumption(count: int = 100) -> List[ConsumptionResponse]:
+    if not count:
+      count = 100
+    d = DB()
+    results = d.execute(sql_string='SELECT * FROM CONSUMPTION LIMIT ?;', params=[str(count)], as_dict=False)
+    column_names = ['product', 'interval_start', 'interval_end', 'consumption']
+    consumption = [dict(zip(column_names, row))  
+              for row in results]
+
     return consumption
